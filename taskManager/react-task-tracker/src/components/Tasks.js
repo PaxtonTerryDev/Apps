72f359040dab1461e0c14975bb881e02b40e8a28
Paxton Terry <paxtonterrydev@gmail.com>
2022-11-26 16:16:25 -0700

    Added task tracker project

---
+++ taskManager/react-task-tracker/src/components/Tasks.js
@@ -0,0 +1,16 @@
+import Task from './Task'
+
+const Tasks = ({ tasks, onDelete, onToggle }) => {
+
+  return (
+    <>
+        {tasks.map((tasks, index) => (
+        <Task key={index} 
+        task={tasks} 
+        onDelete={onDelete} onToggle={onToggle} />
+        ))}
+    </>
+  )
+}
+
+export default Tasks
